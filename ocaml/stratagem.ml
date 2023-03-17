fbf4f3e86ea86884c2cc5ee935b12354564cb168
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2023-03-17 15:49:05 +0100

    Refactor apply and lambda

--- ocaml/stratagem.ml
+++ ocaml/stratagem.ml
@@ -16,48 +16,6 @@
 (*                                                                   *)
 (*********************************************************************)
 
-(* An implementation of Call/cc; temporary structure to preserve
-   familiarity with SML/NJ code. *)
-module Callcc: sig
-  type 'a cont
-  val throw : 'a cont -> 'a -> 'b
-  val callcc : ('a cont -> 'a) -> 'a
-  val prompt : (unit -> 'a) -> 'a
-end = struct
-  type empty = |
-  type 'a cont = 'a -> empty
-  type _ Effect.t += Callcc : (('a -> 'b) -> 'a) -> 'a Effect.t
-
-  let throw : 'a cont -> 'a -> 'b
-    = fun k x -> match k x with | _ -> .
-
-  let callcc : (('a -> 'b) -> 'a) -> 'a
-    = fun f -> Effect.perform (Callcc f)
-
-  let rec hprompt : unit -> ('a, 'a) Effect.Deep.handler
-    = fun () ->
-    let open Effect.Deep in
-      { retc = (fun ans -> ans)
-      ; exnc = raise
-      ; effc = (fun (type a) (eff : a Effect.t) ->
-        match eff with
-        | Callcc f ->
-           Some (fun (k : (a, _) continuation) ->
-               let exception Throw of a in
-               let cont : a -> empty
-                 = fun x -> raise (Throw x)
-               in
-               try
-                 prompt (fun () ->
-                     let ans = f (fun x -> match cont x with _ -> .) in
-                     continue k ans)
-               with
-               | Throw x -> continue k x)
-        | _ -> None) }
-  and prompt : (unit -> 'a) -> 'a
-    = fun f -> Effect.Deep.match_with f () (hprompt ())
-end
-
 (* SIGNATURE DECLARATIONS *)
 
 (* Natural numbers with coding functions.
@@ -134,12 +92,12 @@ module type FOREST = sig
    val apply  : forest -> forest -> forest
    val lambda : (forest -> forest) -> forest
    val reset  : unit -> bool
-   module A: sig
-    (* type carries = int *)
-    (* type gives = forest *)
-    (* type 'a handler = carries -> (gives -> 'a) -> 'a *)
-     val a_try : (unit -> 'a) -> (int -> (forest -> 'a) -> 'a) -> 'a
-   end
+   (* module A: sig *)
+   (*  (\* type carries = int *\) *)
+   (*  (\* type gives = forest *\) *)
+   (*  (\* type 'a handler = carries -> (gives -> 'a) -> 'a *\) *)
+   (*   val a_try : (unit -> 'a) -> (int -> (forest -> 'a) -> 'a) -> 'a *)
+   (* end *)
 end
 
 module type IRRED = sig
@@ -424,15 +382,55 @@ end = struct
   open Nat.Pair
 
   type forest = Forest of (nat -> (nat * forest))
+  let forest : (nat -> nat * forest) -> forest
+    = fun f -> Forest f
 
   let run (Forest f) n = f n
   let (%) = run
+  let (%%) f i =
+    let (n, f') = f % i in
+    (i, f')
   (* Application operation *)
 
   (* To apply a forest F to a forest G, we interpret the labels on F
      as coding either *questions* to be put to some previously
      obtained subtree of G (identified by a timestamp), or *answers*
      giving the labels on the resulting tree. *)
+
+   (* local *)
+   (*    exception Diverge *)
+   (*    fun diverge () = (print "Diverging...\n" ; raise Diverge) *)
+   (*    fun lookup t [] = diverge ()    (\* diverge if timestamp is unknown *\) *)
+   (*      | lookup t ((t',F)::rest) = *)
+   (*           if t=t' then F else lookup t rest *)
+
+   (*    fun play (forest f) previous timestamp n = *)
+   (*       case f n of (f_label,f_cont) => *)
+   (*          if isAns f_label then *)
+   (*             (deAns f_label,  *)
+   (*              forest (play f_cont previous timestamp)) *)
+   (*          else  *)
+   (*             case proj (deQuest f_label) of (n,t) => *)
+   (*                case (lookup (int t) previous) % n of (m,G) => *)
+   (*                   play f_cont ((timestamp,G)::previous) *)
+   (*                        (timestamp+1) m *)
+   (* in *)
+   (* fun apply F G = forest (play F [(0,G)] 1) *)
+   (* end *)
+
+  let rec apply : forest -> forest -> forest
+    = fun f g ->
+    let rec apply'' : (nat * forest) -> forest -> (nat * forest)
+      = fun (n, f) g ->
+      if is_ans n then
+        let (j, g') = g % n in
+        apply'' (f %% j) g'
+      else
+        (n, apply f g)
+    in
+    forest (fun i -> apply'' (f %% i) g)
+
+  let reset () = failwith "TODO"
 
   (* let apply : forest -> forest -> forest *)
   (*   = let rec apply f g = Forest (fun i -> apply'' (f % i) g) *)
@@ -442,6 +440,44 @@ end = struct
   (* Much harder is the "lambda" operation, which requires
      catchcont3. Some "error handling" is needed here to cope with
      non-linear behaviour where it can't arise. *)
+
+  type _ Effect.t += Branch : nat -> (nat * forest) Effect.t
+  type branch = nat -> nat * forest
+
+  exception Non_linear_error
+  let error_branch : branch
+    = fun _ -> raise Non_linear_error
+
+
+  let lambda : (forest -> forest) -> forest
+    = fun phi ->
+    let rec lambda' : (branch -> forest) -> forest
+      = fun p ->
+      forest
+        (fun h ->
+          lambda''
+            (fun g ->
+              let f = p g in
+              f % h))
+    and lambda'' : (branch -> nat * forest) -> nat * forest
+      = fun r ->
+      Effect.Deep.match_with r (fun q -> Effect.perform (Branch q)) hbranch
+    and hbranch =
+      let open Effect.Deep in
+      { retc = (fun (ans, p') -> (ans, lambda' (fun _ -> p')))
+      ; exnc = raise
+      ; effc = (fun (type a) (eff : a Effect.t) ->
+        match eff with
+        | Branch q ->
+           Some (fun (k : (a, _) continuation) ->
+               (q, forest
+                     (fun (a : nat) ->
+                       lambda''
+                         (fun (br : branch) ->
+                           continue k (a, forest br)))))
+        | _ -> None) }
+    in
+    lambda' (fun f -> phi (forest f))
 
   (* type _ Effect.t += Question : nat -> nat Effect.t *)
   (* let question n = Effect.perform (Question n) *)
@@ -462,165 +498,165 @@ end = struct
 
   (* let reset = failwith "TODO" *)
 
-  let apply f g =
-    let exception Diverge in
-    let diverge () = print_endline "Diverging..."; raise Diverge in
-    let rec lookup t = function
-      | [] -> diverge () (* diverge if timestamp is unknown *)
-      | (t', f) :: rest ->
-         if t = t' then f else lookup t rest
-    in
-    let rec play (Forest f) previous timestamp n =
-      let (f_label, f_cont) = f n in
-      if is_ans f_label then (de_ans f_label, Forest (play f_cont previous timestamp))
-      else let (n, t) = proj (de_quest f_label) in
-           let (m, g) = run (lookup (int t) previous) n in
-           play f_cont ((timestamp, g) :: previous) (timestamp+1) m
-    in
-    Forest (play f [(0, g)] 1)
+  (* let apply f g = *)
+  (*   let exception Diverge in *)
+  (*   let diverge () = print_endline "Diverging..."; raise Diverge in *)
+  (*   let rec lookup t = function *)
+  (*     | [] -> diverge () (\* diverge if timestamp is unknown *\) *)
+  (*     | (t', f) :: rest -> *)
+  (*        if t = t' then f else lookup t rest *)
+  (*   in *)
+  (*   let rec play (Forest f) previous timestamp n = *)
+  (*     let (f_label, f_cont) = f n in *)
+  (*     if is_ans f_label then (de_ans f_label, Forest (play f_cont previous timestamp)) *)
+  (*     else let (n, t) = proj (de_quest f_label) in *)
+  (*          let (m, g) = run (lookup (int t) previous) n in *)
+  (*          play f_cont ((timestamp, g) :: previous) (timestamp+1) m *)
+  (*   in *)
+  (*   Forest (play f [(0, g)] 1) *)
 
-  (* Abstraction operation *)
-  type lambda_stamp = unit ref
+  (* (\* Abstraction operation *\) *)
+  (* type lambda_stamp = unit ref *)
 
-  (* Q-exceptions: restartable exceptions for generating question nodes *)
-  module Q = struct
-    type 'a cont = 'a Callcc.cont
-    type carries = nat * int
-    type gives = nat * forest
-    type result = gives
-    type handler = carries -> (gives -> (unit -> result) cont -> result) -> result
-    type exit = lambda_stamp * (unit -> result) cont
+  (* (\* Q-exceptions: restartable exceptions for generating question nodes *\) *)
+  (* module Q = struct *)
+  (*   type 'a cont = 'a Callcc.cont *)
+  (*   type carries = nat * int *)
+  (*   type gives = nat * forest *)
+  (*   type result = gives *)
+  (*   type handler = carries -> (gives -> (unit -> result) cont -> result) -> result *)
+  (*   type exit = lambda_stamp * (unit -> result) cont *)
 
-    let exit_list = ref ([] : exit list)
-    exception Q_E of lambda_stamp * carries * gives cont * exit list
-    let rec lookup s = function
-      | [] -> (None, [])
-      | (s', k) :: rest ->
-         if s = s' then (Some k, rest)
-         else match lookup s rest with
-              | (v, rest') -> (v, (s', k) :: rest')
+  (*   let exit_list = ref ([] : exit list) *)
+  (*   exception Q_E of lambda_stamp * carries * gives cont * exit list *)
+  (*   let rec lookup s = function *)
+  (*     | [] -> (None, []) *)
+  (*     | (s', k) :: rest -> *)
+  (*        if s = s' then (Some k, rest) *)
+  (*        else match lookup s rest with *)
+  (*             | (v, rest') -> (v, (s', k) :: rest') *)
 
-    let extract_exit s =
-      match lookup s !exit_list with
-      | (v, exits) -> exit_list := exits; v
+  (*   let extract_exit s = *)
+  (*     match lookup s !exit_list with *)
+  (*     | (v, exits) -> exit_list := exits; v *)
 
-    let return_to s f =
-      match extract_exit s with
-      | None -> f
-      | Some k -> Callcc.throw k f
+  (*   let return_to s f = *)
+  (*     match extract_exit s with *)
+  (*     | None -> f *)
+  (*     | Some k -> Callcc.throw k f *)
 
-    let q_e_return (s, (s', x', k', pending)) =
-      match extract_exit s with
-      | None -> (fun () -> raise (Q_E (s', x', k', pending)))
-      | Some k -> Callcc.throw k (fun () -> raise (Q_E (s', x', k', (s, k) :: pending)))
+  (*   let q_e_return (s, (s', x', k', pending)) = *)
+  (*     match extract_exit s with *)
+  (*     | None -> (fun () -> raise (Q_E (s', x', k', pending))) *)
+  (*     | Some k -> Callcc.throw k (fun () -> raise (Q_E (s', x', k', (s, k) :: pending))) *)
 
-    let reset () =
-      match !exit_list with
-      | [] -> true
-      | _ -> (exit_list := []; false)
+  (*   let reset () = *)
+  (*     match !exit_list with *)
+  (*     | [] -> true *)
+  (*     | _ -> (exit_list := []; false) *)
 
 
-    exception Q_exn of lambda_stamp * carries * (result -> result)
+  (*   exception Q_exn of lambda_stamp * carries * (result -> result) *)
 
-    let q_try' : lambda_stamp -> (unit -> result) -> handler -> (unit -> result)
-      = fun stamp code handler ->
-      let answer =
-        try code () with
-        | Q_E (s, x, k, pending) ->
-           if s <> stamp
-           then raise (Q_E (s, x, k, pending))
-           else let resume y new_exit =
-                  let open Callcc in
-                  exit_list := (stamp, new_exit) :: pending @ !exit_list;
-                  callcc (fun _k' -> throw k y)
-                in handler x resume
-      in
-      try
-        return_to stamp (fun () -> answer)
-      with
-      | Q_exn (s, x, k) ->
-         return_to s (fun () -> raise (Q_exn (s, x, k)))
-      | Q_E (s, x, k, pending) -> q_e_return (s, (s, x, k, pending))
-      | other -> return_to stamp (fun () -> raise other)
+  (*   let q_try' : lambda_stamp -> (unit -> result) -> handler -> (unit -> result) *)
+  (*     = fun stamp code handler -> *)
+  (*     let answer = *)
+  (*       try code () with *)
+  (*       | Q_E (s, x, k, pending) -> *)
+  (*          if s <> stamp *)
+  (*          then raise (Q_E (s, x, k, pending)) *)
+  (*          else let resume y new_exit = *)
+  (*                 let open Callcc in *)
+  (*                 exit_list := (stamp, new_exit) :: pending @ !exit_list; *)
+  (*                 callcc (fun _k' -> throw k y) *)
+  (*               in handler x resume *)
+  (*     in *)
+  (*     try *)
+  (*       return_to stamp (fun () -> answer) *)
+  (*     with *)
+  (*     | Q_exn (s, x, k) -> *)
+  (*        return_to s (fun () -> raise (Q_exn (s, x, k))) *)
+  (*     | Q_E (s, x, k, pending) -> q_e_return (s, (s, x, k, pending)) *)
+  (*     | other -> return_to stamp (fun () -> raise other) *)
 
-    let q_try : lambda_stamp -> (unit -> result) -> result
-      = fun stamp code ->
-      q_try' stamp code
-        (fun t c ->
-          let open Callcc in
-          let d x = callcc (fun k () -> c x k) () in
-          raise (Q_exn (stamp, t, d))) ()
+  (*   let q_try : lambda_stamp -> (unit -> result) -> result *)
+  (*     = fun stamp code -> *)
+  (*     q_try' stamp code *)
+  (*       (fun t c -> *)
+  (*         let open Callcc in *)
+  (*         let d x = callcc (fun k () -> c x k) () in *)
+  (*         raise (Q_exn (stamp, t, d))) () *)
 
-    let q_raise : lambda_stamp -> carries -> result
-      = fun stamp x ->
-      Callcc.callcc (fun (k : gives Callcc.cont) -> raise (Q_E (stamp, x, k, [])))
-  end
-  let reset = Q.reset
+  (*   let q_raise : lambda_stamp -> carries -> result *)
+  (*     = fun stamp x -> *)
+  (*     Callcc.callcc (fun (k : gives Callcc.cont) -> raise (Q_E (stamp, x, k, []))) *)
+  (* end *)
+  (* let reset = Q.reset *)
 
-   (* A-exceptions: simple restartable exceptions allowing us to change
-      our mind about the argument of a function in mid-computation. *)
-  module A = struct
-    type 'a cont = 'a Callcc.cont
-    type carries = int
-    type gives = forest
-    type 'a handler = carries -> (gives -> 'a) -> 'a
-    exception A_E of carries * gives cont
+  (*  (\* A-exceptions: simple restartable exceptions allowing us to change *)
+  (*     our mind about the argument of a function in mid-computation. *\) *)
+  (* module A = struct *)
+  (*   type 'a cont = 'a Callcc.cont *)
+  (*   type carries = int *)
+  (*   type gives = forest *)
+  (*   type 'a handler = carries -> (gives -> 'a) -> 'a *)
+  (*   exception A_E of carries * gives cont *)
 
-    let a_try : (unit -> 'a) -> 'a handler -> 'a
-      = fun code handler ->
-      try code () with
-      | A_E (x, k) -> handler x (Callcc.throw k)
+  (*   let a_try : (unit -> 'a) -> 'a handler -> 'a *)
+  (*     = fun code handler -> *)
+  (*     try code () with *)
+  (*     | A_E (x, k) -> handler x (Callcc.throw k) *)
 
-    let a_raise : carries -> 'b
-      = fun x -> Callcc.callcc (fun k -> raise (A_E (x,k)))
+  (*   let a_raise : carries -> 'b *)
+  (*     = fun x -> Callcc.callcc (fun k -> raise (A_E (x,k))) *)
 
-  (* A little recursion to smuggle a forest F into the computation of F'.
-     Timestamps are used to distinguish between different A_raise:s.
+  (* (\* A little recursion to smuggle a forest F into the computation of F'. *)
+  (*    Timestamps are used to distinguish between different A_raise:s. *)
 
-     Known inefficiency: we get nested calls to A_plugin accumulating
-     as we move down the forest (this happens even with lambda id).
-     Getting rid of these should be possible, but not trivial. *)
+  (*    Known inefficiency: we get nested calls to A_plugin accumulating *)
+  (*    as we move down the forest (this happens even with lambda id). *)
+  (*    Getting rid of these should be possible, but not trivial. *\) *)
 
-    let rec a_plugin : carries -> forest -> forest -> forest
-      = fun t f f' ->
-      Forest (fun m ->
-          a_try
-            (fun () ->
-              let (i, f'') = run f' m in
-              (i, a_plugin t f f''))
-            (fun t' c ->
-              if t' = t
-              then c f
-              else c (a_raise t')))
+  (*   let rec a_plugin : carries -> forest -> forest -> forest *)
+  (*     = fun t f f' -> *)
+  (*     Forest (fun m -> *)
+  (*         a_try *)
+  (*           (fun () -> *)
+  (*             let (i, f'') = run f' m in *)
+  (*             (i, a_plugin t f f'')) *)
+  (*           (fun t' c -> *)
+  (*             if t' = t *)
+  (*             then c f *)
+  (*             else c (a_raise t'))) *)
 
-  end
+  (* end *)
 
-  (* The abstraction operation itself *)
-  let lambda : (forest -> forest) -> forest
-    = fun phi ->
-    let stamp = ref () in
-    let q_try = Q.q_try stamp in
-    let q_raise = Q.q_raise stamp in
-    (* the main recursion *)
-    let rec lambda' : int -> (forest -> forest) -> forest
-      = fun timestamp phi ->
-      Forest (fun m ->
-          try
-            (match (q_try (fun () ->
-                        phi (A.a_try
-                               (fun () -> A.a_raise timestamp)
-                               (fun t c ->
-                                 if t = timestamp
-                                 then c (Forest (fun n -> q_raise (n, t)))
-                                 else c (A.a_raise t))) % m))
-             with
-             | (a, f') -> (ans a, lambda' timestamp (fun f -> A.a_plugin timestamp f f')))
-          with
-          | Q.Q_exn (s, (n, timestamp'), c) ->
-             if s <> stamp
-             then raise (Q.Q_exn (s, (n, timestamp'), c))
-             else let q = quest (pair n (nat timestamp)) in
-                  (q, lambda' (timestamp+1)
-                        (fun f -> Forest (fun p -> c (p, f)))))
-    in lambda' 0 phi
+  (* (\* The abstraction operation itself *\) *)
+  (* let lambda : (forest -> forest) -> forest *)
+  (*   = fun phi -> *)
+  (*   let stamp = ref () in *)
+  (*   let q_try = Q.q_try stamp in *)
+  (*   let q_raise = Q.q_raise stamp in *)
+  (*   (\* the main recursion *\) *)
+  (*   let rec lambda' : int -> (forest -> forest) -> forest *)
+  (*     = fun timestamp phi -> *)
+  (*     Forest (fun m -> *)
+  (*         try *)
+  (*           (match (q_try (fun () -> *)
+  (*                       phi (A.a_try *)
+  (*                              (fun () -> A.a_raise timestamp) *)
+  (*                              (fun t c -> *)
+  (*                                if t = timestamp *)
+  (*                                then c (Forest (fun n -> q_raise (n, t))) *)
+  (*                                else c (A.a_raise t))) % m)) *)
+  (*            with *)
+  (*            | (a, f') -> (ans a, lambda' timestamp (fun f -> A.a_plugin timestamp f f'))) *)
+  (*         with *)
+  (*         | Q.Q_exn (s, (n, timestamp'), c) -> *)
+  (*            if s <> stamp *)
+  (*            then raise (Q.Q_exn (s, (n, timestamp'), c)) *)
+  (*            else let q = quest (pair n (nat timestamp)) in *)
+  (*                 (q, lambda' (timestamp+1) *)
+  (*                       (fun f -> Forest (fun p -> c (p, f))))) *)
+  (*   in lambda' 0 phi *)
 end
